c2d6730fa0f8e3d8bcfc024a123e24546c15678a
Henri <henri.mynttinen1@gmail.com>
2018-02-18 19:27:35 +0200

    4.8-9 refactored operations to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,30 +1,36 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
+blogsRouter.get('/', async (request, response) => {
+    try {
+        const blogs = await Blog
+            .find({})
+        
         response.json(blogs)
-      })
+    } catch (exception) {
+        console.log(exception)
+        response.status(400).send({ error: 'failed to fetch blogs'})
+    }
 })
   
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+    try {
+        const blog = new Blog(request.body)
     
-    if (blog.title === undefined || blog.url === undefined) {
-        return response.status(400).json({ error: 'title or url missing' })
-    }
+        if (blog.title === undefined || blog.url === undefined) {
+            return response.status(400).json({ error: 'title or url missing' })
+        }
 
-    if (blog.likes === undefined) {
-        blog.likes = 0
+        if (blog.likes === undefined) {
+            blog.likes = 0
+        }
+
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    } catch (exception) {
+        console.log(exception)
+        response.status(500).json({ error: 'failed to add new blog'})
     }
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-    })
 })
 
 module.exports = blogsRouter
