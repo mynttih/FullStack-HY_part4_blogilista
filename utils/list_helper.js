49121745d074c852f0fbd1e5e7231190b5dc9e09
Henri <henri.mynttinen1@gmail.com>
2018-02-18 10:31:26 +0200

    osa4.3-5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,26 @@ const dummy = (blogs) => {
     return 1
 }
 
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.reduce((blog1, blog2) => {
+        return blog1.likes > blog2.likes ? blog1 : blog2
+    })
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
+const totalLikes = (blogs) => {
+    const total = blogs.reduce((sum, blog) => {
+        return sum + blog.likes
+    }, 0)
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    favoriteBlog,
+    totalLikes
 }
